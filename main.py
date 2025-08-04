689e8f85b77f99266ce89c47a88f50134d393c84
Chris <Britten63@hotmail.com>
2025-08-04 11:36:44 -0300

    Fixed Desc

---
+++ main.py
@@ -0,0 +1,28 @@
+#Date: 08-04-2025
+#Author(s): Robot Group 3
+
+# Justin Seaward, Christopher Britten, Matt Reid, Deng Ring, Keith Bishop.
+
+#Description: This Program simulates operations of HAB taxi services, 
+#the program will allow the user to intereact with the company menu system.
+#--------------------
+# Enter New Emploee 
+#--------------------
+# Enter Company Revenue 
+#--------------------
+# Enter Company Expense 
+#--------------------
+# Track Car Rental 
+#--------------------
+# Record Employee Pay 
+#--------------------
+# Print Profit Listing
+#--------------------
+# Print Driver Finance 
+#--------------------
+# Exit Program 
+#--------------------
+
+# The Program will use defaults.dat to store constant data. 
+
+# There will be a sample data file created for faker data, 
