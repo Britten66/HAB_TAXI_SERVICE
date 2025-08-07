97fc64ba7998ecd525b699c52ba7988ae69cfbd7
MattReid1982 <mattwcreid@hotmail.com>
2025-08-07 11:15:35 -0230

    changes made

--- main.py
+++ main.py
@@ -7,6 +7,40 @@
 #--------------------
 # Enter New Emploee 
 #--------------------
+# Author: Robot Group 3
+# Date: August 6th, 2025
+import datetime
+
+try:
+    with open('Default.dat', 'r') as file:
+        NEXT_TRANS_NUM = file.read()
+        NEXT_DRIVER_NUM = file.read()
+        MONTHLY_STAND_FEE = file.read() #For Drivers with their own vehicle.
+        DAILY_RENT_FEE = file.read() #For drivers that rent one of the company cars.
+        WEEKLY_RENT_FEE = file.read() # " "
+
+except FileNotFoundError:
+    print("Error: Default.dat file not found.")
+
+Employee_ID = input("Enter the employee ID: ")
+First_Name = input("Enter the employee first name: ").upper()
+Last_Name = input("Enter the employee last name: ").upper()
+Street_Add = input("Enter the employee street address:")
+Phone = input("Enter the employee phone number[(XXX) XXX-XXXX]: ")
+formatted_phone = f"({Phone[0:3]}) {Phone[3:6]}-{Phone[6:10]}"
+Driver_Num = int(input("Enter the driver number: "))
+# Driver_Ex_Date = 
+Ins_Comp = input("What is the name of the insurance company?: ").upper()
+Pol_Num = int(input("Enter the policy number for the driver: "))
+Own_Vehicle = input("Does the driver own his own vehicle? (Y/N): ").upper()
+# if Own_Vehicle == "Y":
+
+# Bal_Due_Stand = 
+
+
+
+
+
 
 #--------------------
 # Enter Company Revenue 
