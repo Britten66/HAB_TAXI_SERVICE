0ed14dd819c1cda8f58ae92a71aaedce2d975f9e
MattReid1982 <mattwcreid@hotmail.com>
2025-08-09 15:07:22 -0230

    Added a new Employees.dat file and added the variables to print to the .dat file.

--- main.py
+++ main.py
@@ -33,54 +33,56 @@ while True:
 
     def enternewemployee():
         Employee_ID = input("Enter the employee ID: ")
-
         First_Name = input("Enter the employee first name: ").upper()
         Last_Name = input("Enter the employee last name: ").upper()
         Street_Add = input("Enter the employee street address:")
         Phone = input("Enter the employee phone number[(XXX) XXX-XXXX]: ")
-        formatted_phone = format_phone.format(Phone)
+        formatted_phone = FV.format_phone.format(Phone)
         
         # Employee information.
         Driver_Num = int(input("Enter the driver number: "))
-    
-        Driver_Ex_Date = FDateS.format(2028, 12, 20) #Year / month / day of the expiration date.
+    # Start checking this section here! 
+        Driver_Ex_Date = FV.FDateS.format(2028, 12, 20) #Year / month / day of the expiration date.
 
 
-        Driver_Ex_Date = date(2028, 12, 20) # Year / month / day of the expiration date.
+        Driver_Ex_Date = FV.date(2028, 12, 20) # Year / month / day of the expiration date.
         if datetime.today() > Driver_Ex_Date:
             print("The driver's license is expired.") 
             exit()
+    # Check this section for improvement!
             
-    Ins_Comp = input("What is the name of the insurance company?: ").upper()
-    Pol_Num = int(input("Enter the policy number for the driver: "))
-    Own_Vehicle = input("Does the driver own his own vehicle? (Y/N): ").upper()
-
-                   
+        Ins_Comp = input("What is the name of the insurance company?: ").upper()
+        Pol_Num = int(input("Enter the policy number for the driver: "))
+        Own_Vehicle = input("Does the driver own his own vehicle? (Y/N): ").upper()
+        
+        with open("Employees.dat", "a") as f:
+            f.write(f"{Employee_ID},{First_Name},{Last_Name},{Street_Add},{formatted_phone},{Driver_Num},{Driver_Ex_Date},{Ins_Comp},{Pol_Num},{Own_Vehicle}\n")
+                        
 #--------------------
 # Track Car Rental 
 #--------------------
-    # Rental vehicle number for the user to select.
-    Rental_Vehicle_Num = [1, 2, 3, 4]
-    Rent_Period = ["day", "week"]
+        # Rental vehicle number for the user to select.
+        Rental_Vehicle_Num = [1, 2, 3, 4]
+        Rent_Period = ["day", "week"]
 
-    if Own_Vehicle == "Y":
-        print("Driver's vehicle used.")
-        
-    else:
-        # Ask the user for rental details.
-        Rent_Choice = int(input("Enter a vehicle rental number (1-4): "))
-        Rent_Period = input("How long does the driver need the rental? (day / week): ").upper()
-        if Rent_Choice in [1, 2, 3, 4]:
-            print(f"Rental vehicle number {Rent_Choice} selected.")
+        if Own_Vehicle == "Y":
+            print("Driver's vehicle used.")
+            
         else:
-            print("Invalid rental vehicle number.")
+            # Ask the user for rental details.
+            Rent_Choice = int(input("Enter a vehicle rental number (1-4): "))
+            Rent_Period = input("How long does the driver need the rental? (day / week): ").upper()
+            if Rent_Choice in [1, 2, 3, 4]:
+                print(f"Rental vehicle number {Rent_Choice} selected.")
+            else:
+                print("Invalid rental vehicle number.")
 
-        if Rent_Period == "DAY":
-            print('Rental period set to daily.')
-        elif Rent_Period == "WEEK":
-            print("Rental period set to weekly.")
-        else:
-            print("Invalid rental period.")
+            if Rent_Period == "DAY":
+                print('Rental period set to daily.')
+            elif Rent_Period == "WEEK":
+                print("Rental period set to weekly.")
+            else:
+                print("Invalid rental period.")
 
 #--------------------
 # Record Employee Pay 
@@ -89,6 +91,7 @@ while True:
 #--------------------
 # Print Profit Listing
 #--------------------
+
 #--------------------
 # Menu Picking Portion Here 
 #--------------------
